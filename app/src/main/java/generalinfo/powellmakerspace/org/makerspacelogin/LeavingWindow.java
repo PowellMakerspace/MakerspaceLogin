4861285b24fc496c1de70e31d25bcb134940b199
alanmerritt <merrittalan1@gmail.com>
2018-06-12 20:43:26 -0600

    Added leaving window.

---
+++ app/src/main/java/generalinfo/powellmakerspace/org/makerspacelogin/LeavingWindow.java
@@ -0,0 +1,16 @@
+package generalinfo.powellmakerspace.org.makerspacelogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * The window for when the user leaves the MakerSpace.
+ */
+public class LeavingWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaving_window);
+    }
+}
