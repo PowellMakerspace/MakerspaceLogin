5f9257d60a4a334826642f876e71ce018abcb9ff
Powell-Makerspace <generalinfo@powellmakerspace.org>
2018-06-30 16:15:44 -0600

    QR Code Implemented -QR Code is generated and displayed for new members -QR Code Reader used member-facing camera to read QR Code
    -QR Code Reader tested using computer, smartphone, and paper.
    
    Still need to implement sending the QR Code to our member via email

--- app/src/main/java/generalinfo/powellmakerspace/org/makerspacelogin/LoginWindows/LoginMethodWindow.java
+++ app/src/main/java/generalinfo/powellmakerspace/org/makerspacelogin/LoginWindows/LoginMethodWindow.java
@@ -1,14 +1,21 @@
 package generalinfo.powellmakerspace.org.makerspacelogin.LoginWindows;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import generalinfo.powellmakerspace.org.makerspacelogin.R;
 
 public class LoginMethodWindow extends AppCompatActivity {
+
+    long member_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +24,17 @@ public class LoginMethodWindow extends AppCompatActivity {
 
         // Launch QR Code Reader
         Button qrcodeButton = (Button) findViewById(R.id.qrcodeButton);
+        final Activity activity = this;
         qrcodeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent launchQRCodeReader = new Intent(getApplicationContext(), QRCodeWindow.class);
-                startActivity(launchQRCodeReader);
-                finish();
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Scan Code");
+                integrator.setCameraId(1);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
             }
         });
 
@@ -36,5 +48,37 @@ public class LoginMethodWindow extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode,data);
+        if (result != null){
+            if (result.getContents()==null){
+                Toast.makeText(this,"You cancelled the scanning",Toast.LENGTH_LONG).show();
+            }
+            else{
+                String[] resultText = result.getContents().toString().split(": ");
+                if(resultText[0].equals("MS ID")){
+                    //Get and parse member ID from QR Code Reader
+                    Toast.makeText(this, "Your ID is:" + resultText[1], Toast.LENGTH_LONG).show();
+                    member_id = Long.parseLong(resultText[1]);
+
+                    //Continue Login System using member ID
+                    Intent launchMemberConfirmWindow = new Intent(getApplicationContext(), MemberConfirmWindow.class);
+                    launchMemberConfirmWindow.putExtra("org.powellmakerspace.generalinfo.MEMBER_ID", member_id);
+                    startActivity(launchMemberConfirmWindow);
+                    finish();
+                }
+                else{
+                    Toast.makeText(this,"Not a Makerspace Code",Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        else {
+            super.onActivityResult(resultCode, resultCode, data);
+        }
+
+
     }
 }
--- app/src/main/java/generalinfo/powellmakerspace/org/makerspacelogin/LoginWindows/NewMemberWindow.java
+++ app/src/main/java/generalinfo/powellmakerspace/org/makerspacelogin/LoginWindows/NewMemberWindow.java
@@ -1,13 +1,21 @@
 package generalinfo.powellmakerspace.org.makerspacelogin.LoginWindows;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import generalinfo.powellmakerspace.org.makerspacelogin.MainApplication.DatabaseHelper;
 import generalinfo.powellmakerspace.org.makerspacelogin.Classes.Member;
@@ -22,7 +30,9 @@ public class NewMemberWindow extends AppCompatActivity {
     RadioButton membershipRadioButton;
     String memberName, membershipType;
 
+    Button nextButton;
     Button createMemberButton;
+    ImageView QRDisplayImageView;
 
     long memberId;
 
@@ -38,7 +48,10 @@ public class NewMemberWindow extends AppCompatActivity {
 
         membershipRadioGroup = (RadioGroup) findViewById(R.id.membershipRadioGroup);
 
+        nextButton = (Button) findViewById(R.id.nextButton);
         createMemberButton = (Button) findViewById(R.id.createMemberButton);
+
+        QRDisplayImageView = (ImageView) findViewById(R.id.QRDisplayImageView);
 
 
         createMemberButton.setOnClickListener(new View.OnClickListener() {
@@ -59,6 +72,26 @@ public class NewMemberWindow extends AppCompatActivity {
                 // Add new member to the database
                 memberId = makerspaceDatabase.createMember(newMember);
 
+                // Create and display QR Code
+
+                if (memberId > -1) {
+                    MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+
+                    try {
+                        BitMatrix bitMatrix = multiFormatWriter.encode("MS ID: " + memberId, BarcodeFormat.QR_CODE, 500, 500);
+                        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                        Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                        QRDisplayImageView.setImageBitmap(bitmap);
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 // Create Intent
                 Intent launchSurveyWindow = new Intent(getApplicationContext(), SurveyWindow.class);
 
